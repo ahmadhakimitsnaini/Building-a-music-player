01a282859ed9eec5671ab90b51657292cb6a05b0
frumpfruit <fernandabekasi@gmail.com>
2024-10-04 10:45:06 +0700

    Building a music player step 30

--- script.js
+++ script.js
@@ -89,13 +89,38 @@ let userData = {
   songCurrentTime: 0,
 };
 
+//Function to display the songs in the UI (User Interface)
 const renderSongs = (array) => {
-  const songsHTML = array.map((song) => {
-    return `
+  //map method berguna untuk membuat array baru dengan operasi tertentu tanpa harus melakukan perubahan pada array lama.
+  const songsHTML = array
+    .map((song) => {
+      return `
         <li id="song-${song.id}" class="playlist-song">
-  
-        
+        <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+         </button>
+        <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
         </li>
         `;
+    })
+    .join(""); // The join() method is used to concatenate all the elements of an array into a single string. It takes an optional parameter called a separator which is used to separate each element of the array.
+  playlistSongs.innerHTML = songsHTML;
+};
+
+//  Function sort these music list in alphabetical order by title.
+const sortSongs = () => {
+  // sort the songs in alphabetical order by title, and pass in a compare callback function into sort() method.
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+      //The reason why this example is returning numbers is because the sort() method is expecting a number to be returned. If you return a negative number, the first item is sorted before the second item.
+    }
   });
 };
+
+renderSongs(userData?.songs);
